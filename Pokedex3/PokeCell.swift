743b61b6ea21fd2817ab31765680ae94ad9b3118
Juha Kesti <juha.kesti@Juhas-MacBook-Pro-161.local>
2017-01-15 13:17:00 +0200

    added PokeCell class

---
+++ Pokedex3/PokeCell.swift
@@ -0,0 +1,20 @@
+//
+//  PokeCell.swift
+//  Pokedex3
+//
+//  Created by Juha Kesti on 15/01/17.
+//  Copyright © 2017 nauski. All rights reserved.
+//
+
+import UIKit
+
+class PokeCell: UICollectionViewCell {
+    
+    @IBOutlet weak var thumbImg: UIImageView!
+    @IBOutlet weak var nameLbl: UILabel!
+    
+    var pokemon: Pokemon!
+    
+    
+    
+}
